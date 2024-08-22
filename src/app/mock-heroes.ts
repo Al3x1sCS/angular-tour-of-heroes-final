fe7fa230e690113368553ceaa0de9b5d1e86ef08
Al3x1sCS <prog.alexis.cs@protonmail.com>
2024-08-21 23:59:26 -0300

    fix: agora as imagens aparecem

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -7,7 +7,7 @@ export const HEROES: Hero[] = [
     origem: 'Durante a pandemia de 2020, quando o mundo inteiro foi forçado a se adaptar ao trabalho remoto, um jovem e talentoso gerente de projetos chamado João Silva descobriu que tinha um talento especial para organizar e conduzir reuniões virtuais. Ele conseguia unir equipes dispersas pelo mundo e resolver conflitos com uma eficiência impressionante. Certo dia, após uma maratona de reuniões, João foi atingido por um raio enquanto estava conectado a uma chamada de vídeo. Em vez de sofrer danos, ele ganhou poderes extraordinários',
     power: 'Super Reunião a Distância - Cria uma reunião obrigatória com todos os heróis e vilões para decidir uma resolução pacífica.',
     arqui_inimigo: 'Dr. Desconexão, com o poder, Caos Digital onde pode lançar ataques cibernéticos que desativam plataformas de trabalho remoto, forçando todos a resolverem problemas cara a cara.',
-    imageUrl: 'https://i.postimg.cc/MK8LW8t4/H1-Capitao-Home-Office.png'
+    imageUrl: 'assets/images/heroes/01.png'
   },
   {
       id: 2,
@@ -15,7 +15,7 @@ export const HEROES: Hero[] = [
       origem: 'João, sempre foi um viajante curioso e aventureiro. Desde jovem, ele sonhava em explorar o mundo e descobrir lugares mágicos e misteriosos. Em uma de suas viagens, enquanto caminhava por uma floresta densa e antiga, João encontrou uma fonte mágica escondida entre as árvores. A fonte brilhava com uma luz suave e encantadora, e João, fascinado, decidiu fazer um desejo. Ele desejou ter o poder de se teletransportar para poder explorar o mundo de forma mais rápida e eficiente. No entanto, a fonte, com seu senso de humor peculiar, concedeu-lhe o poder de se teletransportar apenas 1 cm de cada vez.',
       power: 'O poder surpreendente de se teletransportar incríveis 1cm.',
       arqui_inimigo: 'Dr. Inércia, um cientista que tem o poder de controle da inércia, permitindo que ele pare completamente qualquer objeto em movimento ou o acelere a velocidades perigosas.',
-      imageUrl: 'https://i.postimg.cc/s216nfDB/H2-Desloca-Pouco.png'
+      imageUrl: 'assets/images/heroes/02.png'
     },
   {
       id: 3,
@@ -23,7 +23,7 @@ export const HEROES: Hero[] = [
       origem: 'Lucas Silva sempre foi fascinado por química desde criança. Cresceu em uma pequena cidade onde passava horas no laboratório da escola, experimentando com diferentes substâncias. Após se formar em química, conseguiu um emprego em uma empresa de produtos de limpeza, onde desenvolvia novas fórmulas para sabões e detergentes. Um dia, enquanto trabalhava em uma nova fórmula de sabão, Lucas acidentalmente misturou alguns ingredientes de maneira errada. O resultado foi uma substância que criava bolhas de sabão extremamente resistentes. Curioso, ele começou a explorar as propriedades dessa nova substância e descobriu que as bolhas eram praticamente inquebráveis. \n Percebendo o potencial dessa descoberta, Lucas decidiu usar suas habilidades para o bem. Ele criou um traje especial que lhe permitia gerar e controlar essas bolhas de sabão inquebráveis. Adotando o nome de “Escudo Espumoso”, ele começou a usar suas habilidades para proteger os outros e combater o crime de maneiras criativas e inesperadas.',
       power: 'Pode criar bolhas de sabão que não estouram.',
       arqui_inimigo: 'Vácuo Voraz, ele teria a habilidade de criar vórtices de vácuo que sugam as bolhas de sabão, deslocando elas e atrapalhando o posicionamento das bolhas.',
-      imageUrl: 'https://i.postimg.cc/xTY6w31V/H3-Escudo-Espumoso.png'
+      imageUrl: 'assets/images/heroes/03.png'
     },
   {
       id: 4,
@@ -31,7 +31,7 @@ export const HEROES: Hero[] = [
       origem: 'Roberto sempre curioso e apaixonado por tecnologia e espionagem. Desde pequeno, ele sonhava em ser um super-herói, mas nunca imaginou que isso se tornaria realidade de uma forma tão peculiar. Um dia, enquanto explorava um laboratório abandonado, Lucas encontrou um dispositivo estranho. Sem saber exatamente o que era, ele o ativou. O dispositivo emitiu uma luz intensa e, de repente, Lucas sentiu uma mudança em seu corpo. Ele havia adquirido a habilidade de ficar invisível, mas com uma condição: ele só poderia ficar invisível quando ninguém estivesse olhando ou quando não pudesse ser detectado por câmeras.',
       power: 'Ficar invisível, mas apenas quando não pode ser visto ou detectado por câmeras, pelo menos ele sabe quando ninguém o está observando.',
       arqui_inimigo: 'O Vigilante, ele é obcecado por monitorar tudo e todos, usando câmeras e tecnologia avançada para manter o controle.',
-      imageUrl: 'https://i.postimg.cc/SsCG5n0K/H4-Visivel.png'
+      imageUrl: 'assets/images/heroes/04.png'
     },
   {
       id: 5,
@@ -39,6 +39,6 @@ export const HEROES: Hero[] = [
       origem: 'Em uma cidade vibrante e cheia de vida, onde a música e a cultura pop dominam, nasceu uma fã incondicional de todas as coisas relacionadas a shows e eventos. \n Desde pequena, ela era fascinada por concertos, festivais e qualquer coisa que envolvesse uma plateia animada. Seu nome verdadeiro é desconhecido, mas ela sempre foi chamada de “Super Fã” por seus amigos devido à sua paixão inabalável. \n Um dia, durante um show de sua banda favorita, algo extraordinário aconteceu. No meio da multidão, uma misteriosa luz brilhou intensamente, e uma estranha energia foi canalizada para sua lightstick. A partir desse momento, ela descobriu que podia soltar raios laser poderosos da lightstick, capazes de criar confusão e discórdia onde quer que ela fosse.',
       power: 'Soltar raio laser da lightstick, criar confusão e discórdia por onde passa.',
       arqui_inimigo: 'Maestro do Caos, que tem o poder da Sinfonia do Caos, com sua batuta encantada, ele pode controlar as emoções e ações das pessoas ao seu redor, criando uma sinfonia de caos e desordem.',
-      imageUrl: 'https://i.postimg.cc/brPRjq89/H5-Super-Fa.png'
+      imageUrl: 'assets/images/heroes/05.png'
     }
 ];
