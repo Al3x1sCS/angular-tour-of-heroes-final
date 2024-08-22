fac2d77933cbdb18b57ab354a7d2f6cdb663596e
Aline-Agostinho <alineab.agostinho@gmail.com>
2024-08-22 08:44:18 -0300

    Componente Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, FooterComponent],
+  imports: [RouterOutlet, HeroesComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
